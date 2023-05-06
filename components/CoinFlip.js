d82b742807f99bf640c66ed7a08a7516403b2754
Nathan <hello@nathandorney.co.uk>
2023-05-06 12:00:08 +0100

    first commit

---
+++ components/CoinFlip.js
@@ -0,0 +1,95 @@
+"use client";
+import { Coin, Info, Plus, PlusCircle, Warning, X } from "phosphor-react";
+import { useState, useEffect } from "react";
+
+function CoinFlip() {
+  const [options, setOptions] = useState(["Heads", "Tails"]);
+  const [flipResult, setFlipResult] = useState("");
+  const [isFlipping, setIsFlipping] = useState(false);
+
+  const addOption = () => {
+    setOptions([...options, ""]);
+  };
+
+  const handleOptionChange = (index, value) => {
+    const newOptions = [...options];
+    newOptions[index] = value;
+    setOptions(newOptions);
+  };
+
+  const removeOption = (index) => {
+    const newOptions = [...options];
+    newOptions.splice(index, 1);
+    setOptions(newOptions);
+  };
+
+  const flipCoin = () => {
+    if (options.every((option) => option.trim() !== "")) {
+      setIsFlipping(true);
+      setTimeout(() => {
+        const result = options[Math.floor(Math.random() * options.length)];
+        setFlipResult(result);
+        setIsFlipping(false);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    setFlipResult("");
+  }, [options]);
+
+  return (
+    <div className="coin-container">
+      <h1>Enter options:</h1>
+      {options.map((option, index) => (
+        <div key={index} className="option">
+          <input
+            type="text"
+            placeholder="Enter an option..."
+            value={option}
+            onChange={(e) => handleOptionChange(index, e.target.value)}
+          />
+          {options.length > 2 && (
+            <button onClick={() => removeOption(index)}>
+              <X size={20} weight="light" className="x" />
+            </button>
+          )}
+        </div>
+      ))}
+
+      <div className="btn-container">
+        <div className="btn-wrapper">
+          <button onClick={addOption} className="btn secondary ">
+            <Plus size={18} weight="light" />
+            Add Option
+          </button>
+          <button
+            onClick={flipCoin}
+            disabled={
+              isFlipping || options.some((option) => option.trim() === "")
+            }
+            className="btn primary"
+          >
+            <Coin size={20} weight="light" />
+            {isFlipping ? "Flipping..." : "Flip coin"}
+          </button>
+        </div>
+        <div className={`result ${isFlipping ? "flip" : ""}`}>
+          {flipResult && (
+            <div className="answer">
+              <h2>{flipResult}</h2>
+            </div>
+          )}
+        </div>
+        {options.some((option) => option.trim() === "") && (
+          <div className="error">
+            <Info size={20} weight="light" />
+            <p>All options must be filled in to flip the coin</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default CoinFlip;
